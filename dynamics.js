e934818dc4b3cb447af61a0bdbbaab3d11c60dd2
tennisplayer106 <76907264+tennisplayer106@users.noreply.github.com>
2023-03-02 23:11:42 +0530

    Add files via upload

---
+++ dynamics.js
@@ -0,0 +1,16 @@
+function teleportation(){
+    window.open( "achievements.html","_blank");
+}
+
+function teleportation1(){
+    window.open( "about_me.html","_blank");
+}
+
+// function teleportation2(){
+   // window.open( "https://docs.google.com/document/d/1Uo-ZEbUFNhtniWriYoW-OOEviJWKnTu_oy7zoV2HGFU/edit","_blank");
+// }
+
+// function teleportation3(){
+ //   window.open( "https://docs.google.com/document/d/1_YNoqsOxPm7kaHP2SSzUAbYhc2ih2SauBbB_52eZeL4/edit","_blank");
+// }
+
